7c04bb8fea2860b7e2d39dcf672fb751aafc6f55
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-05-18 18:30:12 -0400

    w1 home

--- WS01/w1-at_home/w1.cpp
+++ WS01/w1-at_home/w1.cpp
@@ -1,8 +1,6 @@
-#ifndef SICT_STRING_H
-#define SICT_STRING_H
 /*
  ============================================================================
- Name        : String.h
+ Name        : w1.cpp
  Author      : Adam Stinziani
  Email       : astinziani@myseneca.ca
  Student #   : 124521188
@@ -15,20 +13,34 @@
 
  /*
   ============================================================================
-  Description : String class
+  Description : main function and global variable
   ============================================================================
  */
 #include <iostream>
+#include "process.h"
 
-namespace sict {
-	const int MAX = 3;
-	class String {
-		char* string;
-	public:
-		String(const char* string_n);
-		~String();
-		void display(std::ostream& o) const;
-	};
-	std::ostream& operator<<(std::ostream&, String& source);
-}
-#endif // !SICT_STRING_H
+extern const int INITIAL = 3;
+
+	using namespace std;
+
+	int main(int argCount, char** args) {
+
+		int control = 0;
+
+		if (argCount > 0) {
+			cout << "Command Line : ";
+			for (int i = 0; i < argCount; i++) {
+				cout << args[i] << ' ';
+			}
+			cout << endl;
+			for (int i = 1; i < argCount; i++) {
+				sict::process(args[i]);
+			}
+		}
+
+		else {
+			cout << "***Insufficient number of arguments***";
+			control = 1;
+		}
+		return control;
+	}
