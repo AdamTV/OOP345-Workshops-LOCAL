caefc10446066ecc62efac8d50acc0ca0d20eadb
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-05-20 17:08:23 -0400

    w1 rename

---
+++ 1/lab/W1_LAB/String.cpp
@@ -0,0 +1,57 @@
+#define _CRT_SECURE_NO_WARNINGS
+/*
+ ============================================================================
+ Name        : String.cpp
+ Author      : Adam Stinziani
+ Email       : astinziani@myseneca.ca
+ Student #   : 124521188
+ Course Code : OOP345
+ Section     : SCC
+ Date        : 2019-05-15
+ Workshop    : Workshop 1 - Lab
+ ============================================================================
+ */
+
+ /*
+  ============================================================================
+  Description : String class
+  ============================================================================
+ */
+
+#include <cstring>
+#include "String.h"
+
+extern const int INITIAL;
+int initial = INITIAL;
+
+namespace sict {
+
+	String::String(const char* string_n)
+	{
+		if (string_n != nullptr) {
+			int length = strlen(string_n);
+			string = new char[length];
+			strncpy(string, string_n, length);
+			string[length] = '\0';
+		}
+		else {
+			string = nullptr;
+		}
+	}
+	String::~String()
+	{
+		delete [] string;
+		string = nullptr;
+	}
+	void String::display(std::ostream& o) const
+	{
+		o << string;
+	}
+	std::ostream& operator<<(std::ostream& o, String& source)
+	{
+		
+		o << initial++ << ": ";
+		source.display(o);
+		return o;
+	}
+}
---
+++ 1/lab/W1_LAB/w1.cpp
@@ -0,0 +1,46 @@
+/*
+ ============================================================================
+ Name        : w1.cpp
+ Author      : Adam Stinziani
+ Email       : astinziani@myseneca.ca
+ Student #   : 124521188
+ Course Code : OOP345
+ Section     : SCC
+ Date        : 2019-05-15
+ Workshop    : Workshop 1 - Lab
+ ============================================================================
+ */
+
+ /*
+  ============================================================================
+  Description : main function and global variable
+  ============================================================================
+ */
+#include <iostream>
+#include "process.h"
+
+extern const int INITIAL = 3;
+
+	using namespace std;
+
+	int main(int argCount, char** args) {
+
+		int control = 0;
+
+		if (argCount > 0) {
+			cout << "Command Line : ";
+			for (int i = 0; i < argCount; i++) {
+				cout << args[i] << ' ';
+			}
+			cout << endl;
+			for (int i = 1; i < argCount; i++) {
+				sict::process(args[i]);
+			}
+		}
+
+		else {
+			cout << "***Insufficient number of arguments***";
+			control = 1;
+		}
+		return control;
+	}
