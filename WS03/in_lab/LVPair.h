db379b6a3d7659998ff1d766a26b7fd9088d9ac6
AdamTV <43098728+AdamTV@users.noreply.github.com>
2019-05-30 17:04:58 -0400

    3lab

--- WS03/in_lab/LVPair.h
+++ WS03/in_lab/LVPair.h
@@ -1,9 +1,41 @@
 #ifndef SICT_LVPAIR_H
 #define SICT_LVPAIR_H
+/*
+ ============================================================================
+ Name        : LVPair.h
+ Author      : Adam Stinziani
+ Email       : astinziani@myseneca.ca
+ Student #   : 124 521 188
+ Course Code : OOP345
+ Section     : SCC
+ Date        : 2019-05-30
+ Workshop    : Workshop 3 - Lab
+ ============================================================================
+ */
 
+ /*
+  ============================================================================
+  Description : LVPair class template definition
+  ============================================================================
+ */
+#include <iostream>
 
-
-
-
+namespace sict {
+	template <typename L, typename V>
+	class LVPair {
+		L currentLabel;
+		V currentValue;
+	public:
+		//No argument constructor to set safe empty state
+		LVPair() : currentLabel{}, currentValue{} {}
+		//Constructor to set LVPair values
+		LVPair(const L& label, const V& value): currentLabel(label), currentValue(value) {}
+		//display Label value pair using any ostream
+		void display(std::ostream& os) const { os << currentLabel << " : " << currentValue << std::endl; }
+	};
+	// overloaded extraction operator to display label-value pair
+	template<typename L, typename V>
+	std::ostream& operator<<(std::ostream& os, const LVPair<L, V>& pair) { pair.display(os); return os; }
+}
 #endif // !SICT_LVPAIR_H
 
--- WS03/in_lab/List.h
+++ WS03/in_lab/List.h
@@ -1,11 +1,48 @@
 #ifndef SICT_LIST_H
 #define SICT_LIST_H
+/*
+ ============================================================================
+ Name        : List.h
+ Author      : Adam Stinziani
+ Email       : astinziani@myseneca.ca
+ Student #   : 124 521 188
+ Course Code : OOP345
+ Section     : SCC
+ Date        : 2019-05-30
+ Workshop    : Workshop 3 - Lab
+ ============================================================================
+ */
 
+ /*
+  ============================================================================
+  Description : List class template definition
+  ============================================================================
+ */
 
-
-
-
-
-
+namespace sict {
+	
+	template <typename T, size_t N>
+	class List {
+		T a [N];
+		//Initialize listSize to 0 to start at beggining of array
+		size_t listSize = 0;
+	public:
+		//Returns the current size of the array
+		size_t size() const {
+			return listSize;
+		}
+		//subscript operator to make values in array accessible
+		const T& operator[](size_t i) const {
+			return a[i];
+		}
+		//overloaded += operator to add an element to the array if there is room
+		void operator+=(const T& t) {
+			if (listSize < N) {
+				a[listSize] = t;
+				listSize++;
+			}
+		}
+	};
+}
 #endif // !SICT_LIST_H
 
